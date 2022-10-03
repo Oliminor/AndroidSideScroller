13d533c6cc8dbdc04b0ec2cf95036b4a1bf29fb6
Oliminor <Oliminor@users.github.com>
2022-10-03 19:35:45 +0100

    Small spawner changes

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,24 +11,24 @@ public class EnemySpawner : MonoBehaviour
 
     List<EnemySpawnData> inActiveObjectPool = new();
     List<EnemySpawnData> activeObjectPool = new();
-
-    float timeBetweenSpawn;
     void Start()
     {
         InstiateObjectToPool();
+        StartCoroutine(ObjectSpawner(spawnRate));
     }
 
     // Update is called once per frame
     void Update()
     {
         SwapObjects();
+    }
 
-        timeBetweenSpawn += Time.deltaTime;
-
-        if (spawnRate <= timeBetweenSpawn)
+    IEnumerator ObjectSpawner(float _spawnrate)
+    {
+        while(true)
         {
-            timeBetweenSpawn = 0;
             Spawner();
+            yield return new WaitForSeconds(_spawnrate);
         }
     }
 
@@ -40,7 +40,7 @@ public class EnemySpawner : MonoBehaviour
             {
                 GameObject go = Instantiate(spawnableObjects[i].spawnObject, enemyParentObject);
                 go.SetActive(false);
-                inActiveObjectPool.Add(new EnemySpawnData(go, spawnableObjects[i].spawnPosY, 0));
+                inActiveObjectPool.Add(new EnemySpawnData(go, spawnableObjects[i].spawnPosY, 0, spawnableObjects[i].spawnRotation));
             }
         }
     }
@@ -56,9 +56,11 @@ public class EnemySpawner : MonoBehaviour
         if (inActiveObjectPool[randomIndex].spawnPosY != 0) spawnPos = new Vector3(spawnPos.x, inActiveObjectPool[randomIndex].spawnPosY, spawnPos.z);
 
         Vector3 spwanPosWorldToPoint = Camera.main.ViewportToWorldPoint(spawnPos);
+        Vector3 spawnRotation = inActiveObjectPool[randomIndex].spawnRotation;
 
         inActiveObjectPool[randomIndex].spawnObject.gameObject.transform.position = spwanPosWorldToPoint;
         inActiveObjectPool[randomIndex].spawnObject.SetActive(true);
+        inActiveObjectPool[randomIndex].spawnObject.gameObject.transform.rotation = Quaternion.Euler(spawnRotation);
         activeObjectPool.Add(inActiveObjectPool[randomIndex]);
         inActiveObjectPool.RemoveAt(randomIndex);
     }
@@ -88,11 +90,13 @@ public class EnemySpawnData
     public GameObject spawnObject;
     public float spawnPosY;
     public int amountOnMap;
+    public Vector3 spawnRotation;
 
-    public EnemySpawnData (GameObject _spawnObject, float _spawnPosY, int _amountOnMap)
+    public EnemySpawnData (GameObject _spawnObject, float _spawnPosY, int _amountOnMap, Vector3 _spawnRotation)
     {
         spawnObject = _spawnObject;
         spawnPosY = _spawnPosY;
         amountOnMap = _amountOnMap;
+        spawnRotation = _spawnRotation;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] float speed;
     public static GameManager instance;
+
+    [SerializeField] float speed;
+    [SerializeField] int score;
+    private GameObject player;
     // Start is called before the first frame update
 
+    public void SetSpeed(float _speed) { speed = _speed; } 
     public float GetSpeed() { return speed; }
+    public void SetPlayer(GameObject _player) { player = _player; }
+    public GameObject GetPlayer() { return player; }
+    public void SetScore(int _score) { score = _score; }
+    public int GetScore() { return score; }
 
     void Awake()
     {
         instance = this;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
